f121e747189d5dbdc5a08d9c8831ca030c424f31
DELL-PC\DELL <s0920832252@yahoo.com.tw>
2017-10-20 18:16:53 +0800

    完成

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(function () {
         }
     });
     $(".text").each(function (i) {
-        let len = 180; // 超過180個字以"..."取代
+        let len = 200; // 超過180個字以"..."取代
         if ($(this).text().length > len) {
             //            $(this).attr("title", $(this).text());
             var text = $(this).text().substring(0, len - 1) + " ...";
